098cdf34295a383868d9f47910b06428369c9c88
23ji <isj4194@naver.com>
2025-07-06 16:07:25 +0900

    BMI 모델 생성

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 이상지 on 7/6/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+import UIKit
+
+struct BMI {
+  let bmi: Float
+  let advice: String
+  let backgroundColor: UIColor
+}
