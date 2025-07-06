de5578143bf781c5e57d17e2fe4f829eb19fdb6c
23ji <isj4194@naver.com>
2025-07-06 16:03:36 +0900

    properties 연결

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CalculateViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+  @IBOutlet weak var heightLabel: UILabel!
+  @IBOutlet weak var heightSilder: UISlider!
+  
+  @IBOutlet weak var weightLabel: UILabel!
+  @IBOutlet weak var weightSilder: UISlider!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+  @IBAction func heightSilderChanged(_ sender: UISlider) {
+  }
+  @IBAction func weightSilderChanged(_ sender: UISlider) {
+  }
+  @IBAction func calculateButton(_ sender: UIButton) {
+  }
+}
+
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by 이상지 on 7/6/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+  @IBOutlet weak var bmiLabel: UILabel!
+  
+  @IBOutlet weak var adviceLabel: UILabel!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  @IBAction func recalculateButton(_ sender: UIButton) {
+  }
+}
